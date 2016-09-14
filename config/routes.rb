8c42a408696893e2bcc3dbf275f10346715966ea
[01;31m[K"name"[m[K: "Andrew Peterson <ajpeterson79@gmail.com>
2016-09-14 09:29:18 -0700

    Add styling for links in sidebar once logged in.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#index'
 
+  resources :pages, only: [:index]
   resources :listings, only: [:index, :show]
 
   resources :users, only: [:new, :create, :show] do
