f6b241d2d1d4259bfaf2134d68724f06faaa4374
[01;31m[K"name"[m[K: "Andrew Peterson <ajpeterson79@gmail.com>
2016-09-13 22:13:38 -0700

    Add validation tests for user model.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe 'User' do
+  let(:valid_user) do User.create(
+    name: 'Brian',
+    email: 'brian@yahoo.com',
+    password: 'password')
+  end
+
+  let(:invalid_user) do User.create(
+    name: '',
+    email: '',
+    password: '')
+  end
+
+  describe 'validations' do
+    describe 'when valid' do
+      it 'validates the presence of name' do
+        expect(valid_user.name).to eq('Brian')
+      end
+
+      it 'validates the presence of email' do
+        expect(valid_user.email).to eq('brian@yahoo.com')
+      end
+
+      it 'validates the presence of password' do
+        expect(valid_user.password).to eq('password')
+      end
+    end
+
+    describe 'when not valid' do
+      it 'is not valid without a name, email and password' do
+        expect(invalid_user).to_not be_valid
+      end
+    end
+  end
 end
