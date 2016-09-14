e2678eaa27969e4a4bc02b1ea851316fc934a1b5
[01;31m[K"name"[m[K: "Andrew Peterson <ajpeterson79@gmail.com>
2016-09-14 08:32:58 -0700

    Resolve REST routing issue. Add show, new and create.

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,11 +1,31 @@
 class ListingsController < ApplicationController
   def index
-    @listings = Listing.order(:created_at).limit(5)
+    @user = User.find(params[:user_id])
+    @listings = @user.listings.all
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @listing = @user.listings.new(listing_params)
+
+    if @listing.save
+      redirect_to user_listing_url(user_id: @listing.user.id, id: @listing.id)
+    else
+      render 'new'
+    end
+  end
+
+  def new
+    @listing = Listing.new
   end
 
   def show
+    @user = User.find(params[:user_id])
     @listing = Listing.find(params[:id])
+  end
 
-    render 'show'
+  private
+  def listing_params
+    params.require(:listing).permit(:address, :city, :state, :zip, :description)
   end
 end
