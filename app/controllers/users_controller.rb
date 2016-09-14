76bfa9b66ff2604b61ccecdfb342363544614454
[01;31m[K"name"[m[K: "Andrew Peterson <ajpeterson79@gmail.com>
2016-09-14 01:34:06 -0700

    Still working on CRUD.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  def index
+    @user = User.find(params[:user_id])
+    @listings = @user.listings.order(:created_at).limit(5)
+
+    render :'users/user_id/listings'
+  end
+
   def new
     @user = User.new
   end
