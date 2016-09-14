d27f70a031d0a99e603ef948470d4152bf90649b
[01;31m[K"name"[m[K: "Andrew Peterson <ajpeterson79@gmail.com>
2016-09-14 09:08:30 -0700

    Finish CRUD for listings when logged-in.

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -4,6 +4,10 @@ class ListingsController < ApplicationController
     @listings = @user.listings.all
   end
 
+  def new
+    @listing = Listing.new
+  end
+
   def create
     @user = User.find(params[:user_id])
     @listing = @user.listings.new(listing_params)
@@ -15,13 +19,34 @@ class ListingsController < ApplicationController
     end
   end
 
-  def new
-    @listing = Listing.new
-  end
-
   def show
     @user = User.find(params[:user_id])
     @listing = Listing.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:user_id])
+    @listing = Listing.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:user_id])
+    @listing = @user.listings.find(params[:id])
+
+    if @listing.update(listing_params)
+      redirect_to user_listing_url(user_id: @listing.user.id, id: @listing.id)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:user_id])
+    @listing = @user.listings.find(params[:id])
+
+    @listing.destroy
+
+    redirect_to user_listings_url(user_id: @listing.user.id)
   end
 
   private
